4c55ad2e75d0d51750314e67ff65ab85680b9415
Rodrigo <rodweb21@gmail.com>
2025-04-02 19:26:56 -0500

    register

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.HelloView.as_view(), name='hello'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -3,6 +3,13 @@ from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
+from rest_framework.decorators import api_view
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+
 class HelloView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
@@ -11,3 +18,30 @@ class HelloView(APIView):
 
     def get(self, request):
         return Response({'message': 'Hello, authenticated user!'})
+
+
+@api_view(['POST'])
+def login(request):
+    return Response({})
+
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        user = User.objects.get(username=serializer.data['username'])
+        user.set_password(serializer.data['password'])
+        user.save()
+
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def profile(request):
+    return Response({})
