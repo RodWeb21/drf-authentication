a317fec3795c928f44cf4673c522fdf66f3b538d
Rodrigo <rodweb21@gmail.com>
2025-04-02 19:38:44 -0500

    login

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -8,6 +8,7 @@ from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 class HelloView(APIView):
@@ -22,7 +23,16 @@ class HelloView(APIView):
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
